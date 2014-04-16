ceaced6bc687f748ec376f22f043898931f477fe
liaohqiu <liaohuqiu@gmail.com>
2014-04-16 12:09:30 +0800

    merge from sandyzhang's branch

--- files/vimfiles/colors/my_color.vim
+++ files/vimfiles/colors/my_color.vim
@@ -4,7 +4,6 @@
 " Last Change:	$Date: 2004/06/13 19:30:30 $
 " URL:		http://hans.fugal.net/vim/colors/desert.vim
 " Version:	$Id: desert.vim,v 1.1 2004/06/13 19:30:30 vimboss Exp $
-
 " cool help screens
 " :he group-name
 " :he highlight-groups
--- setup.sh
+++ setup.sh
@@ -7,14 +7,17 @@ function exe_cmd() {
 root_dir=`pwd`
 
 vim_alias_dir=$HOME/.vim
-if[ -e $vim_alias_dir ];then
-    d=`date +%Y%m%d`
-    cp -R  $vim_alias_dir  $HOME/vim_${d} 
-    cp $HOME/.vimrc  $HOME/vimrc_${d}
-fi 
-if [ -e $vim_alias_dir ]; then
-    exe_cmd "rm -r $vim_alias_dir"
-fi
-exe_cmd "ln -sf $root_dir/files/vimfiles  $vim_alias_dir"
-exe_cmd "ln -sf $root_dir/files/_vimrc $HOME/.vimrc"
+vim_rc=$HOME/.vimrc
+d=`date +%Y%m%d-%H%M%S`
 
+if [ -e $vim_alias_dir ]; then
+    exe_cmd "cp -R  $vim_alias_dir  $HOME/vim_${d}"
+    exe_cmd "rm -r $vim_alias_dir"
+fi 
+
+if [ -e $vim_rc ]; then
+    exe_cmd "mv $vim_rc  $HOME/vimrc_${d}"
+fi
+
+exe_cmd "ln -sf $root_dir/files/vimfiles  $vim_alias_dir"
+exe_cmd "ln -sf $root_dir/files/_vimrc $vim_rc"
